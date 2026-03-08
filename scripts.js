d35045b56f16051b0d42045671de8afd8aba0732
Brian Argenti <111089738+BrianArgenti@users.noreply.github.com>
2026-03-07 19:22:42 -0600

    adds button and color changing functionality to project

--- scripts.js
+++ scripts.js
@@ -1,17 +1,29 @@
 function makeGrid(size) {
     let grid = document.querySelector('.grid');
+    grid.innerHTML = ''; // Clear current grid
     for (let i=0; i < size; i++) {
         let column = document.createElement('div');
         column.classList.add('column');
         for (let j=1; j <= size; j++) {
             let row = document.createElement('div');
             row.classList.add('row');
-            row.style.border = `1px solid black`;
-            row.innerText = (i * size) + j;
+            // row.style.border = `1px solid black`;
+            // row.innerText = (i * size) + j;
             column.appendChild(row);
         }
         grid.appendChild(column);
     }
 }
+
+// const createSize = document.querySelector('.main-container');
+// const create = document.createElement('button');
+// create.classList.add('create-btn');
+// create.innerText = 'Create!';
+// createSize.appendChild(create);
+
+document.querySelector('.create-btn').addEventListener('click', () =>{
+    let size = parseInt(prompt('Enter grid size:'), 10);
+    makeGrid(size);
+});
 
 makeGrid(16);
